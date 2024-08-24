2e6cc98fa536955aed45f9dfcf551784f34d0093
leonardoklestadtluz <klestadt@hotmail.com>
2024-08-23 21:40:58 -0300

    12. [SQL Server] Funções Condicionais: conclusão da aula 13. Explicação Exercícios

--- sql-impressionador/aulas.sql
+++ sql-impressionador/aulas.sql
@@ -1965,15 +1965,46 @@ SELECT
 	) AS 'Cliente'
 FROM
 	DimCustomer
+
+-- IIF Composto
+--SELECT * FROM DimProduct
+
+
+-- Existem 3 tipos de estoque: High, Mid e Low. Faça um SELECT contendo as colunas de ProductKey, ProductName, StockTypeName e Nome do responsável pelo produto, de acordo com o tipo de estoque. A regra deverá ser a seguinte:
+
+-- João é responsável pelos produtos High
+-- Maria é responsável pelos produtos Mid
+-- Luis é responsável pelos produtos Low
+
+SELECT 
+	ProductKey,
+	ProductName,
+	StockTypeName,
+	IIF(
+		StockTypeName = 'High',
+		'João',
+		IIF(
+			StockTypeName = 'Mid',
+			'Maria',
+			'Luis'
+		)
+	) AS 'Responsável'
+FROM 
+	DimProduct
+
+
+
+-- ISNULL: Tratando valores nulos
+SELECT * FROM DimGeography
+
+SELECT 
+	GeographyKey,
+	ContinentName, 
+	CityName,
+	ISNULL(CityName, 'Local Desconhecido')
+FROM
+	DimGeography
 */
-
-
-
-
-
-
-
-
 
 
 
