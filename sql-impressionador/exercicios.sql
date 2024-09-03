364ca632f32680df99b526a313be271e1bd8d12e
leonardoklestadtluz <klestadt@hotmail.com>
2024-09-03 18:09:00 -0300

    14. [SQL Server] CRUD: conclusão do exercício 4

---
+++ sql-impressionador/exercicios.sql
@@ -0,0 +1,112 @@
+USE Exercicios
+-- USE BDImpressionador
+/*	14. [SQL Server] CRUD
+
+-- 1. a) Crie um banco de dados chamado BD_Teste.
+CREATE DATABASE BD_Teste
+
+-- b) Exclua o banco de dados criado no item anterior.
+DROP DATABASE BD_Teste-- c) Crie um banco de dados chamado Exercicios.CREATE DATABASE Exercicios
+
+
+/*	2. No banco de dados criado no exercício anterior, crie 3 tabelas, cada uma contendo as seguintes colunas:
+Tabela 1: dCliente
+- ID_Cliente
+- Nome_Cliente
+- Data_de_Nascimento*/CREATE TABLE dCliente(	ID_Cliente INT,
+	Nome_Cliente VARCHAR(100),
+	Data_de_Nascimento DATETIME)/*Tabela 2: dGerente
+- ID_Gerente
+- Nome_Gerente
+- Data_de_Contratacao
+- Salario*/CREATE TABLE dGerente(	ID_Gerente INT,
+	Nome_Gerente VARCHAR(100),
+	Data_de_Contratacao DATETIME,
+	Salario FLOAT)/*Tabela 3: fContratos
+- ID_Contrato
+- Data_de_Assinatura
+- ID_Cliente
+- ID_Gerente
+- Valor_do_Contrato*/CREATE TABLE fContratos(	ID_Contrato INT,
+	Data_de_Assinatura DATETIME,
+	ID_Cliente INT,
+	ID_Gerente INT,
+	Valor_do_Contrato FLOAT)/*Lembre-se dos seguintes pontos:
+
+a) Garantir que o Banco de Dados Exercicios está selecionado.
+b) Definir qual será o tipo de dados mais adequado para cada coluna das tabelas. Lembrando que os tipos de dados mais comuns são: INT, FLOAT, VARCHAR e DATETIME.
+Por fim, faça um SELECT para visualizar cada tabela.*/SELECT * FROM dClienteSELECT * FROM dGerente
+SELECT * FROM fContratos
+
+-- 3. Em cada uma das 3 tabelas, adicione os seguintes valores:
+
+-- TABELA dCliente
+INSERT INTO dCliente(ID_Cliente, Nome_Cliente, Data_de_Nascimento)
+VALUES
+	(1,  'André Martins',   '12/02/1989'),
+	(2,  'Bárbara Campos',  '07/05/1992'),
+	(3,  'Carol Freitas',   '23/04/1985'),
+	(4,  'Diego Cardoso',   '11/10/1994'),
+	(5,  'Eduardo Pereira', '09/11/1988'),
+	(6,  'Fabiana Silva',   '02/09/1989'),
+	(7,  'Gustavo Barbosa', '27/06/1993'),
+	(8,  'Helen Viana',     '11/02/1990'),
+	(9,  'Igor Castro',     '21/08/1989'),
+	(10, 'Juliana Pires',   '12/01/1991')
+
+-- TABELA dGerente
+INSERT INTO dGerente(ID_Gerente, Nome_Gerente, Data_de_Contratacao, Salario)
+VALUES
+	(1, 'Lucas Sampaio',   '21/03/2015', 6700),
+	(2, 'Mariana Padilha', '10/01/2011', 9900),
+	(3, 'Nathália Santos', '03/10/2018', 7200),
+	(4, 'Otávio Costa',    '18/01/2017', 11000)
+
+-- TABELA fContratos
+INSERT INTO fContratos(ID_Contrato, Data_de_Assinatura, ID_Cliente, ID_Gerente, Valor_do_Contrato)
+VALUES
+	(1,	 '12/01/2019', 8,  1, 23000),
+	(2,	 '10/02/2019', 3,  2, 15500),
+	(3,	 '07/03/2019', 7,  2, 6500),
+	(4,	 '15/03/2019', 1,  3, 33000),
+	(5,	 '21/03/2019', 5,  4, 11100),
+	(6,	 '23/03/2019', 4,  2, 5500),
+	(7,	 '28/03/2019', 9,  3, 55000),
+	(8,	 '04/04/2019', 2,  1, 31000),
+	(9,	 '05/04/2019', 10, 4, 3400),
+	(10, '05/04/2019', 6,  2, 9200)
+
+SELECT * FROM dCliente
+SELECT * FROM dGerente
+SELECT * FROM fContratos
+*/
+
+/*	4. Novos dados deverão ser adicionados nas tabelas dCliente, dGerente e fContratos. Fique livre para adicionar uma nova linha em cada tabela contendo, respectivamente:  
+
+(1) um novo cliente (id cliente, nome e data de nascimento) 
+(2) um novo gerente (id gerente, nome, data de contratação e salário) 
+(3) um novo contrato (id, data assinatura, id cliente, id gerente, valor do contrato)
+
+*/
+
+-- TABELA dCliente
+INSERT INTO dCliente(ID_Cliente, Nome_Cliente, Data_de_Nascimento)
+VALUES(11,  'Tunsguistenio Silva', '17/07/1997')
+
+-- TABELA dGerente
+INSERT INTO dGerente(ID_Gerente, Nome_Gerente, Data_de_Contratacao, Salario)
+VALUES(5, 'Astolfo', '29/02/2012', 36700)
+
+-- TABELA fContratos
+INSERT INTO fContratos(ID_Contrato, Data_de_Assinatura, ID_Cliente, ID_Gerente, Valor_do_Contrato)
+VALUES(11, '13/012/2013', 11,  2, 53000)
+
+SELECT * FROM dCliente
+SELECT * FROM dGerente
+SELECT * FROM fContratos
+
+
+
+
+
+
--- sql-impressionador/exerciciosBDImpressionador.sql
+++
@@ -1,83 +0,0 @@
-USE Exercicios
--- USE BDImpressionador
-/*	14. [SQL Server] CRUD
-
--- 1. a) Crie um banco de dados chamado BD_Teste.
-CREATE DATABASE BD_Teste
-
--- b) Exclua o banco de dados criado no item anterior.
-DROP DATABASE BD_Teste-- c) Crie um banco de dados chamado Exercicios.CREATE DATABASE Exercicios
-
-
-/*	2. No banco de dados criado no exercício anterior, crie 3 tabelas, cada uma contendo as seguintes colunas:
-Tabela 1: dCliente
-- ID_Cliente
-- Nome_Cliente
-- Data_de_Nascimento*/CREATE TABLE dCliente(	ID_Cliente INT,
-	Nome_Cliente VARCHAR(100),
-	Data_de_Nascimento DATE)/*Tabela 2: dGerente
-- ID_Gerente
-- Nome_Gerente
-- Data_de_Contratacao
-- Salario*/CREATE TABLE dGerente(	ID_Gerente INT,
-	Nome_Gerente VARCHAR(100),
-	Data_de_Contratacao DATE,
-	Salario FLOAT)/*Tabela 3: fContratos
-- ID_Contrato
-- Data_de_Assinatura
-- ID_Cliente
-- ID_Gerente
-- Valor_do_Contrato*/CREATE TABLE fContratos(	ID_Contrato INT,
-	Data_de_Assinatura DATE,
-	ID_Cliente INT,
-	ID_Gerente INT,
-	Valor_do_Contrato FLOAT)-- ALTERAÇÃO DE DADOS-- ALTER TABLE dCliente ALTER COLUMN Data_de_Nascimento DATETIME-- ALTER TABLE dGerente ALTER COLUMN Data_de_Contratacao DATETIME-- ALTER TABLE fContratos ALTER COLUMN Data_de_Assinatura DATETIME/*Lembre-se dos seguintes pontos:
-
-a) Garantir que o Banco de Dados Exercicios está selecionado.
-b) Definir qual será o tipo de dados mais adequado para cada coluna das tabelas. Lembrando que os tipos de dados mais comuns são: INT, FLOAT, VARCHAR e DATETIME.
-Por fim, faça um SELECT para visualizar cada tabela.*/SELECT * FROM dClienteSELECT * FROM dGerente
-SELECT * FROM fContratos
-*/
-
--- 3. Em cada uma das 3 tabelas, adicione os seguintes valores:
-
--- TABELA dCliente
-INSERT INTO dCliente(ID_Cliente, Nome_Cliente, Data_de_Nascimento)
-VALUES(7, 'Gustavo Barbosa', '1993-06-27')
-	(1, 'André Martins', '1989-02-12'),
-	(2, 'Bárbara Campos', '1992-05-07'),
-	(3, 'Carol Freitas', '1985-04-23'),
-	(4, 'Diego Cardoso', '1994-10-11'),
-	(5, 'Eduardo Pereira', '1988-11-09'),
-	(6, 'Fabiana Silva', '1989-09-02'),
-	(7, 'Gustavo Barbosa', '1993-06-27'),
-	(8, 'Helen Viana', '1990-02-11')
-
--- TABELA dGerente
-INSERT INTO(ID_Gerente, Nome_Gerente, Data_de_Contratacao, Salario)
-VALUES(1, 'Lucas Sampaio', '2015-03-21', 6700)
-	(1, 'Lucas Sampaio', '2015-03-21', 6700),
-	(2, 'Mariana Padilha', '2011-01-10', 9900),
-	(3, 'Nathália Santos', '2018-10-03', 7200),
-	(4, 'Otávio Costa', '2017-01-18', 11000)
-
-SELECT * FROM	dCliente
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
